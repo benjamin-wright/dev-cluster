a40b8a181a48e4b31f5ee43e82da950da53ff58a
Ben Wright <benjamin.wright@nielsen.com>
2020-07-13 13:46:42 +0100

    added elasticsearch

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -129,6 +129,7 @@ function deploy-infra() {
   echo 'deploying standard infrastructure...';
   kubectl get ns | grep infra || kubectl create ns infra
   helm dep update ./infra
+  helm delete infra --namespace infra
   helm upgrade --install infra ./infra \
     --wait \
     --namespace infra \
